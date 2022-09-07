61a139930f3196f1a330d738cd3c3027b39bd9f5
Patrice DecideOm <pmazel@decideom.fr>
2022-09-07 18:47:44 +0200

    Mise en place filtres

--- 01_tabular_sep_2022.jl
+++ 01_tabular_sep_2022.jl
@@ -5,6 +5,7 @@ using MLJ
 using XGBoost
 using Plots
 using TimeSeries
+using Dates
 
 input_path = "C:\\data\\kaggle\\tabular-playground-series-sep-2022"
 
@@ -14,13 +15,26 @@ data_train = CSV.read(input_path * "\\train.csv", DataFrames.DataFrame);
 data_test = CSV.read(input_path * "\\test.csv", DataFrames.DataFrame);
 
 # Structure du DataFrame "data_train"
-# print(schema(data_train))
+print(schema(data_train))
 
 # Données statistiques du DataFrame "data_train"
 print(DataFrames.describe(data_train))
 print("\n")
 
-belgium = subset(data_train, :country => ByRow(==("Belgium")))
+belgium = subset(data_train, :country => ByRow(==("Belgium")) )
+
+dates = [Date(2021,1),Date(2021,2),Date(2021,3)];
+yms = [yearmonth(d) for d in dates];
+
+annee = data_train[in(yms).(yearmonth.(data_train.date)), :]
+
+function complex_filter(country, date)::Bool
+  interesting_country = eval(country == "Belgium")
+  interesting_date = eval(Dates.year(date) == 2020)
+  interesting_country && interesting_date
+end
+
+belgium_filter = filter([:country, :date] => complex_filter, data_train)
 
 # plot(1:n, sold, ylabel="Sold (num.)", label="data", xlabel = "Time (days)")
 #         , xticks=0:365:n)
--- 02_tabular_spe_2022.jl
+++ 02_tabular_spe_2022.jl
@@ -13,6 +13,17 @@ print(input_path * "\n")
 data_train = CSV.read(input_path * "\\train.csv", DataFrames.DataFrame);
 data_test = CSV.read(input_path * "\\test.csv", DataFrames.DataFrame);
 
-ta_train = TimeArray(data_train; timestamp = :date)
+function complex_filter(country, store, product, date)::Bool
+  filter_country = eval(country == "Belgium")
+  filter_store = eval(store == "KaggleMart")
+  filter_product = eval(product == "Kaggle Getting Started")
+  filter_date = eval(Dates.year(date) >= 2019)
+  filter_country && filter_store && filter_product && filter_date
+end
+
+belgium = filter([:country, :store, :product, :date] => complex_filter,
+                  data_train)
+
+ta_train = TimeArray(belgium; timestamp = :date)
 
 plot(ta_train[:num_sold])
